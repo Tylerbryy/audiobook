5b9496acca9e96bc2d5c6427846fa222e4d6ea7a
tylerbry <tylergibbs049@gmail.com>
2023-11-01 15:44:46 -0500

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from bark import generate_audio, preload_models
 from scipy.io.wavfile import write as write_wav
 import numpy as np
 import nltk
-
+import time
+from colorama import Fore, Style
 
 nltk.download('punkt')
 preload_models()
@@ -28,7 +29,7 @@ SAMPLE_RATE = 22050
 HISTORY_PROMPT = "en_speaker_1"
 
 # Path to your file
-file_path = 'Animal Farm (George Orwell) (Z-Library).txt'  
+file_path = r'orangelips.txt'  
 
 # Extract text from file
 long_string = extract_text_from_file(file_path)
@@ -50,18 +51,55 @@ for sentence in sentences:
 # Generate audio for each prompt
 audio_arrays = []
 total_chunks = len(chunks)
+processing_times = []
+
+def format_time(seconds):
+    days, remainder = divmod(seconds, 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    
+    time_str = ""
+    if days > 0:
+        time_str += f"{round(days)} days, "
+    if hours > 0:
+        time_str += f"{round(hours)} hours, "
+    if minutes > 0:
+        time_str += f"{round(minutes)} minutes and "
+    time_str += f"{seconds:.2f} seconds"
+    
+    return time_str
+
 for index, prompt in enumerate(chunks):
-    print(f"Processing chunk {index}: {prompt[:50]}...")  # Print the index and the first 50 characters of the chunk
-    print(f"Chunks left to process: {total_chunks - index - 1}")
+    print(f"{Fore.GREEN}Processing chunk {index}: {prompt[:50]}...{Style.RESET_ALL}")  # Print the index and the first 50 characters of the chunk in green
+    
+    start_time = time.time()
     audio_array = generate_audio(prompt, history_prompt=HISTORY_PROMPT)
+    end_time = time.time()
+
+    processing_time = end_time - start_time
+    processing_times.append(processing_time)
+
+    avg_processing_time = sum(processing_times) / len(processing_times)
+    remaining_chunks = total_chunks - index - 1
+    estimated_time_left = avg_processing_time * remaining_chunks
+
+    minutes, seconds = divmod(estimated_time_left, 60)
+        # Clear the terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+    formatted_time = format_time(estimated_time_left)
+    print(f"{Fore.YELLOW}Estimated time left: {formatted_time}{Style.RESET_ALL}")
+
     audio_arrays.append(audio_array)
-    print(f"Finished processing chunk {index}")
+    print(f"{Fore.BLUE}Finished processing chunk {index}/{total_chunks}{Style.RESET_ALL}")  # Print the finished processing message in blue
+
 
 # Combine the audio files
 combined_audio = np.concatenate(audio_arrays)
 
 # Create a filename based on the settings used
 filename = f"combined_audio_{HISTORY_PROMPT}_{str(len(chunks))}.wav"
+
+print(f"{Fore.CYAN}Writing combined audio to file: {filename}{Style.RESET_ALL}")  # Print the filename in cyan
 
 # Write the combined audio to a file
 write_wav(filename, SAMPLE_RATE, combined_audio)
